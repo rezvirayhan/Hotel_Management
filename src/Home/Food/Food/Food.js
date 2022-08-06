4be83342f10f11c6c03882e64c63b715f3f90164
rezvirayhan <devrezvi.rayhan@gmail.com>
2022-08-06 12:47:12 +0600

    commit

--- src/Home/Food/Food/Food.js
+++ src/Home/Food/Food/Food.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Food = ({food}) => {
-    const {img}= food;
-    return (
-        <div className='col-lg-3 col-md-6 col-sm-12'>
-             <img className='img-thumbnail img-fluid' src={img} alt="" />
-        </div>
-    );
+const Food = ({ food }) => {
+  const {img } = food;
+  return (
+      <div className="col-lg-3 col-md-6 col-sm-12">
+        <img className="img-thumbnail img-fluid" src={img} alt="" />
+      </div>
+  );
 };
 
 export default Food;
