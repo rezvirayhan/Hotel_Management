556cd7e8aa85fc90a8e0c6fd87db4af2d2ac3019
rezvirayhan <devrezvi.rayhan@gmail.com>
2022-08-08 01:15:55 +0600

    commit

--- src/Home/Food/Food/Food.js
+++ src/Home/Food/Food/Food.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Food = ({ food }) => {
-  const {img } = food;
+  const { img, name, price, descripition } = food;
   return (
-      <div className="col-lg-3 col-md-6 col-sm-12">
-        <img className="img-thumbnail img-fluid" src={img} alt="" />
+    <div className="col-lg-3 col-md-6 col-sm-12">
+      <div class="card mb-2 p-1">
+        <img class="card-img-top img-thumbnail" src={img} alt="" />
+        <div class="card-body">
+          <h5 class="card-title">{name}</h5>
+          <p class="card-text">
+           {descripition}
+          </p>
+          <b><h3 className=""> Price: ${price}</h3></b>
+          <Link to={`bye`}>
+            <button className="btn btn-success">Add To Card</button>
+          </Link>
+        </div>
       </div>
+    </div>
   );
 };
 
--- src/Home/Header/Header.js
+++ src/Home/Header/Header.js
@@ -33,8 +33,11 @@ const Header = () => {
             <Nav className="me-auto">
               <Nav.Link href="/ourFood">Our Food</Nav.Link>
               <Nav.Link href="/home#mentors">Mentors</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown title="All-Manu" id="collasible-nav-dropdown">
+                <NavDropdown.Item>
+                <Nav.Link className="text-dark" href="/allReviwe"><b>All Review</b></Nav.Link>
+
+                </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
                 </NavDropdown.Item>
