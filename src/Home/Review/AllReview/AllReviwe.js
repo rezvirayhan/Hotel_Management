c1853a48009dd5c0c9fd3cd033c6b88809f32e37
rezvirayhan <devrezvi.rayhan@gmail.com>
2022-08-07 19:41:41 +0600

    commit

---
+++ src/Home/Review/AllReview/AllReviwe.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+const AllReviwe = () => {
+    useEffect(()=>{
+        
+    },[])
+    return (
+        <div>
+            <h1>This Is All Review Pages</h1>
+        </div>
+    );
+};
+
+export default AllReviwe;
--- src/Home/Review/Review.js
+++ src/Home/Review/Review.js
@@ -4,7 +4,14 @@ import { useForm } from "react-hook-form";
 const Review = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
-    console.log(data);
+   fetch('http://localhost:5000/customerReview',{
+    method:"POST",
+    headers:{"content-type":"application/json"},
+    body:JSON.stringify(data),
+   })
+   .then((res)=> res.json())
+   .then((result)=> console.log(result));
+   console.log(data); 
 
   };
   return (
@@ -12,11 +19,24 @@ const Review = () => {
       <h1>Thsi Is Customer Review Pagess</h1>
       <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
       <input className="mb-3 " placeholder="Name" {...register("firstName", { required: true, maxLength: 20 })} />
-      <textarea rows="5" cols="50" className="mb-3" placeholder="Comment" {...register("lastName", { required: true, maxLength: 20 })} />
-      <input className="mb-3" placeholder="Mark Out Of 10" type="number" {...register("age",)} />
-      <input type="submit" />
+      
+      <textarea rows="5" cols="50" className="mb-3" placeholder="Comment" {...register("comment", { required: true })} />
+
+      <input className="mb-3 " type="number" placeholder="Mark Out Of 10" {...register("Mark", { required: true })} />
+
+
+      <select className="mb-3 p-2" {...register("selected")}>
+        <option value="Good">Good</option>
+        <option value="Best">Best</option>
+        <option value="Better">Better</option>
+        <option value="Vary Good">Very Good</option>
+        <option value="Bad">Bad</option>
+        <option value="Very Bad">Very Bad</option>
+      </select>
+      <input className="mb-3" type="submit" />
     </form>
     </div>
+    
   );
 };
 
